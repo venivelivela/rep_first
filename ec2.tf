e9bbc9814f88db581e8009b278dbe9b3d1c107e7
venivelivela <112842256+venivelivela@users.noreply.github.com>
2022-09-06 15:53:45 +0530

    commit_1

--- ec2.tf
+++ ec2.tf
@@ -15,37 +15,3 @@ resource "aws_instance" "EC2" {
 
 
 }
-resource "aws_security_group" "securitygroup" {
-  name        = "security1"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.VPC.id
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "security1"
-  }
-}
-
-
-
-
